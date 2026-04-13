6b258747ef27928c13f438cc2982edcfb7925d92
USUARIO <Yeidy Caicedo>
2026-04-13 02:45:30 -0500

    Se crea la clase hija Competidor con atributos, constructor y métodos. Se establece el actualizaa el ranking del competidor mediante los puntos y se agrega la información del país al que representan y la cantidad de puntos.

--- src/main/java/Modelo/Competidor.java
+++ src/main/java/Modelo/Competidor.java
@@ -8,6 +8,40 @@ package Modelo;
  *
  * @author USUARIO
  */
-public class Competidor {
-    
+public class Competidor extends Persona {
+    private String paisR;
+    private int puntos;
+    private int rankingMundial;
+
+    public Competidor(String nom, int edad, double estatura, double peso, String paisR, int rankingMundial) {
+        super(nom, edad, estatura, peso);
+        this.puntos = 0;
+        this.rankingMundial = rankingMundial;
+        this.paisR = paisR;
+    }
+
+    public void actualizarRanking(int puntosObtenidos) {
+        this.puntos += puntosObtenidos;
+    }
+
+    public String obtenerDatos() {
+        return rankingMundial + "° |" + super.toString() + "Pais: " + paisR + "\n" + "Puntos: " + puntos;
+    }
+
+    public String getPaisR() {
+        return paisR;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setRankingMundial(int rankingMundial) {
+        this.rankingMundial = rankingMundial;
+    }
+
+    public int getRankingMundial() {
+        return rankingMundial;
+    }
+
 }
