b402b2661c73d69003d17311ae2512789629f3c9
USUARIO <Yeidy Caicedo>
2026-04-13 02:48:40 -0500

    Se crea la clase Competencia con atributos, constructor y métodos. Permite agregar equipos y generar el reporte de la competencia mostrando la clasificación general de los competidores organizandolos en una lista descendente segun su puntaje acumulado.

--- src/main/java/Modelo/Competencia.java
+++ src/main/java/Modelo/Competencia.java
@@ -8,6 +8,85 @@ package Modelo;
  *
  * @author USUARIO
  */
+import java.util.ArrayList;
+
 public class Competencia {
+
+    private String nombreEvento;
+    private ArrayList<Equipo> equipos;
+
+    public Competencia(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+        this.equipos = new ArrayList<>();
+    }
+
+    public void agregarEquipo(Equipo e) {
+        equipos.add(e);
+    }
+
+    public String generarReporte() {
+
+    String reporte = "=== COMPETENCIA: " + nombreEvento + " ===\n\n";
+    reporte += "=== CLASIFICACIÓN GENERAL ===\n\n";
+
+    ArrayList<Competidor> todos = new ArrayList<>();
+
+    // 1. juntar todos los competidores
+    for (Equipo e : equipos) {
+        todos.addAll(e.getCompetidores());
+    }
+
+    // 2. ordenar por ranking (menor = mejor)
+    for (int i = 0; i < todos.size() - 1; i++) {
+        for (int j = i + 1; j < todos.size(); j++) {
+
+            if (todos.get(i).getRankingMundial() > todos.get(j).getRankingMundial()) {
+
+                Competidor temp = todos.get(i);
+                todos.set(i, todos.get(j));
+                todos.set(j, temp);
+            }
+        }
+    }
+
+    // 3. mostrar en orden
+    for (Competidor c : todos) {
+        reporte += "----------------------\n";
+        reporte += c.obtenerDatos();
+    }
+
+    return reporte;
+}
+
     
+    public void calcularRanking() {
+
+    ArrayList<Competidor> todos = new ArrayList<>();
+
+    for (Equipo e : equipos) {
+        todos.addAll(e.getCompetidores());
+    }
+    
+    for (int i = 0; i < todos.size() - 1; i++) {
+        for (int j = i + 1; j < todos.size(); j++) {
+
+            if (todos.get(i).getPuntos() < todos.get(j).getPuntos()) {
+
+                Competidor temp = todos.get(i);
+                todos.set(i, todos.get(j));
+                todos.set(j, temp);
+            }
+        }
+    }
+    int posicion = 1;
+    for (Competidor c : todos) {
+        c.setRankingMundial(posicion);
+        posicion++;
+    }
+}
+
+    public ArrayList<Equipo> getEquipos() {
+        return equipos;
+    }
+
 }
