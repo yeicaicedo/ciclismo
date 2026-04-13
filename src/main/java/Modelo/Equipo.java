50e03649ca328c5d5dfb2e1ce2b488cabfec1f19
USUARIO <Yeidy Caicedo>
2026-04-13 02:47:05 -0500

    Se crea la clase Equipo con atributos, constructor y métodos. Permite crear equipos y agregar competidores, luego obtener la información del equipo.

--- src/main/java/Modelo/Equipo.java
+++ src/main/java/Modelo/Equipo.java
@@ -8,6 +8,43 @@ package Modelo;
  *
  * @author USUARIO
  */
+import java.util.ArrayList;
+
 public class Equipo {
+
+    //Atributos
+    private String nombre;
+    private String pais;
+    private ArrayList<Competidor> competidores;
+
+    public Equipo(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+        this.competidores = new ArrayList<>();
+    }
+
+    public void agregarCompetidor(Competidor c) {
+        competidores.add(c);
+    }
+
+    public String obtenerDatosEquipo() {
+        String datos = "Equipo: " + nombre + " | País: " + pais + "\n";
+        for (Competidor c : competidores) {
+            datos += c.toString() + "\n";
+        }
+        return datos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public ArrayList<Competidor> getCompetidores() {
+        return competidores;
+    }
     
 }
